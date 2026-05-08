fbe8ac2382b24e4d352aa63ccb24b9b95d0475bd
Inapogui-201 <inazezem@gmail.com>
2026-05-08 12:38:54 +0100

    V1

--- client/src/pages/Access.jsx
+++ client/src/pages/Access.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Access = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">Droits d'accès</h1>
+      <h1 className="text-2xl font-bold mb-6">Droits d'accès aux modules</h1>
       <div className="bg-white rounded-lg shadow p-6">
         <p className="text-gray-600">Module Droits d'accès en cours de développement......</p>
       </div>
